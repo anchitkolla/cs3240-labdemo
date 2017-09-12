a69fc75c5137488b5c82d71583227628d62cdb47
Anchit Kolla <anchitkolla@yahoo.com>
2017-09-11 22:50:12 -0400

    Added new multiple message function

--- hello.py
+++ hello.py
@@ -4,3 +4,4 @@ if __name__ == "__main__":
     helper.greeting("hello")
     helper.greeting("bye")
     helper.greeting("hello again")
+    helper.multipleMessage("multiple message",5)
--- helper.py
+++ helper.py
@@ -2,3 +2,7 @@ def greeting(msg):
     print (msg)
     print (msg)
     print (msg)
+
+def multipleMessage(msg,num):
+    for x in range(0,num):
+        print(msg)
